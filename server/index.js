a5aea1d1671979b57080ca409f9bfa31cfa02234
Uday Trivedi <trivediu@gmail.com>
2018-07-19 20:28:24 -0700

    Res Binding Removed

--- server/index.js
+++ server/index.js
@@ -41,14 +41,14 @@ app.post('/saveUser', (req, res, next) => {
     // console.log('POST to /saveUser, req.body is:', req.body.zip)
     // console.log('response to / from server', res);
 
-    let send = res.send.bind(res)
+    //let send = res.send.bind(res)
     let zip = req.body.zip;
 
     apiSearch.searchByZip(zip, (response) => {
       res.status(201);
       //console.log('response.data', response);
       //apiHelpers.getOfficials('state', response);
-      send(apiHelpers.getOfficials('state', response))//is this context ok?
+      res.send(apiHelpers.getOfficials('state', response))//is this context ok?
     });
 
     //uday will add the thing here
