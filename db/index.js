2cbe123586291e64d954af4ee486e685fba963d5
Uday Trivedi <31199718+trivediu@users.noreply.github.com>
2018-07-19 17:44:20 -0700

    Modify Server Index.js To Coordinate With API Helpers and Search Functions (#37)
    * Implement Server Writing Info to DB
    
    * Updated Server DB
    
    * Implement Server Index.js To Synchronize With Helpers

--- db/index.js
+++ db/index.js
@@ -8,4 +8,22 @@ var connection = mysql.createConnection({
   database  : 'greenfield'
 });
 
-module.exports.connection = connection;
+var insertData = (data, callback) => {
+  const username = data.name;
+  const password = data.password;
+  const zipcode = data.zipcode;
+
+  const sql = "INSERT INTO users (username, password, zipcode) VALUES (?, ?, ?)";
+  var inserts = [username, password, zipcode];
+  connection.query(sql, inserts, (err, results, data) => {
+    err ? console.log('Error', err) : callback();
+  });
+};
+
+module.exports = {
+  connection: connection,
+  insertData: insertData
+}
+
+//mysql -u -root > db/schema.sql
+//mysql -u -root -p
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const app = express();
 const db = require('../db/index.js');
 const apiHelpers = require('../lib/apiHelper.js');
 const bodyParser = require('body-parser');
+const apiSearch = require('../lib/apiSearch.js')
 
 app.use(bodyParser.json()) //This should be adjusted towards the type of req.body we will get
 app.use(express.static(__dirname + '/../client/dist'));
@@ -28,21 +29,44 @@ app.listen(port, () => {
 //STATUS: to be integrated with front-end
 app.get('/', (req, res) => {
 
-    console.log('GET / to server', res);
+    // console.log('GET / to server', res);
 
 });
 
 //DESCRIPTION: This will respond to user input on the front-end and send back the appropriate data. req.body will be our friend here.
 //STATUS: to be integrated with front end
-app.post('/saveUser', (req, res) => {
+// app.post('/saveUser', (req, res) => {
+//     //const data = req.body;
+//     console.log(req.body);
+//     const data = {
+//             name: 'Uday Trivedi2',
+//             password: 'ABCDEF2',
+//             zipcode: '917102'
+//            };
 
-    console.log('POST to /, req.body is:', req.body)
-    console.log('response to / from server', res);
+//     const cb = () => {
+//         console.log('success data inserted!');
+//     }
 
-    let sendCallBack = res.send.bind(res)
-    //db.save(req.body, callback)
+//     //db.insertData(data, cb);
 
-})
+// });
+
+app.post('/saveUser', (req, res, next) => {
+
+    // console.log('POST to /, req.body is:', req.body)
+    // console.log('response to / from server', res);
+
+    let send = res.send.bind(res)
+    let zip = req.body //this might need to change depending on its label
+
+    apiSearch.searchByZip(zip, (response) => {
+      res.status(201);
+      send(apiHelpers.getOfficials('state', response.data))//is this context ok?
+    });
+
+    //uday will add the thing here
+});
 
 
 //DESCRIPTION: This post will grab the user form data (zip code currently)
@@ -64,7 +88,7 @@ app.post('/', (req, res) => {
 //STATUS:
 app.post('/', (req, res, next) => {
 
-    console.log('POST to /SOMEOTHERROUTE, req.body is:', req.body)
+    //console.log('POST to /SOMEOTHERROUTE, req.body is:', req.body)
   });
 
 
@@ -72,9 +96,9 @@ app.post('/', (req, res, next) => {
 //STATUS:
 app.put('/', (req, res) => {
 
-    console.log('PUT to /, req.body is:', req.body)
-    console.log('PUT to /, res is:', res)
+    // console.log('PUT to /, req.body is:', req.body)
+    // console.log('PUT to /, res is:', res)
 
 
-    console.log('response to / from server', res);
+    // console.log('response to / from server', res);
 })
