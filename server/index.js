4f8dbed7d53fa3d8b7332f7cf7e2d32dadd863d6
Uday Trivedi <31199718+trivediu@users.noreply.github.com>
2018-07-28 07:33:43 -0700

    Verify Passport Req.user working And Clean Up Passport/Server Code (#57)
    * Correct Server
    
    * Correct Variable Naming To Indicate Data Being Sent and Received (Entire Row Data)
    
    Removed extraneous comments and added some minor documentation
    
    * Verify and Have Tested Auth Routes Working Properly.  Added CheckUser Route
    
    Added a checkUser route for Mubeen to utilize incase we do not use ReactRouter
    
    * Correct Utils Auth Exports to Export Function Correctly.

--- server/index.js
+++ server/index.js
@@ -32,22 +32,9 @@ app.use(cookieParser(config.SESSION_SECRET));
 /************************************************/
 
 app.use(express.static(`${__dirname}/../client/dist`));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-
-
-
-
-// app.use(bodyParser.text()) this is an alternative to json
-
-const db = require('../db/index.js');
-const apiHelpers = require('../lib/apiHelper.js');
-const dataHelpers = require('../lib/dataHelpers.js')
-
-const apiSearch = require('../lib/apiSearch.js');
 
 app.use(bodyParser.json()); // This should be adjusted towards the type of req.body we will get
 // app.use(bodyParser.text()); //or some other type
@@ -84,8 +71,8 @@ app.post('/login', (req, res, next) => {
 app.post('/reps', (req, res, next) => {
 
   console.log("POST to /saveUser, req.body is", req.body);
-  const locater = req.body.zip ? req.body.zip 
-                  : req.body.location ? dataHelpers.abbrState(req.body.location, 'name') 
+  const locater = req.body.zip ? req.body.zip
+                  : req.body.location ? dataHelpers.abbrState(req.body.location, 'name')
                   : null;
   const region = req.body.region;
 
@@ -122,8 +109,14 @@ app.get('/auth/google/callback',
     res.redirect('/');
   });
 
+
 app.get('/test', utils.authCheck, (req, res) => {
-  console.log('passed authCheck')
-  console.log(req.user)
-  res.send(req.user)
-})
+  console.log('passed authCheck');
+  res.send(req.user);
+});
+
+
+//Temp function for Mubeen front-end prior to RR implementation
+app.get('/checkuser', utils.authCheck, (req, res) => {
+  res.send('user exists');
+});
